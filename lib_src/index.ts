0f3db1ff8c3e7922410920f5ce178f424049b064
aminya <aminyahyaabadi74@gmail.com>
2020-03-20 21:53:16 -0500

    imports

---
+++ lib_src/index.ts
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+import { safeLoad } from "js-yaml"
+import { Linter } from "eslint"
+import LintMessage = Linter.LintMessage
+
+const jshint = require("jshint").JSHINT
+
