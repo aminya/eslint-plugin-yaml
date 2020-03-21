af48d309c90f56d269cdb7bddb2d20a9aeb41bef
aminya <aminyahyaabadi74@gmail.com>
2020-03-20 22:15:58 -0500

    Compile

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,7 +4,14 @@ declare function preprocess(text: string, fileName: string): {
     text: string;
     filename: string;
 }[];
-declare function postprocess(messages: LintMessage[][], fileName: string): any;
+declare function postprocess(messages: LintMessage[][], fileName: string): Linter.LintMessage[] | {
+    ruleId: string;
+    severity: number;
+    message: any;
+    source: any;
+    line: any;
+    column: any;
+}[];
 export declare const processors: {
     ".yml": {
         preprocess: typeof preprocess;
