1a1f2569305e1bdcb1fc230c6ae3819e2d329ca4
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-03-09 17:03:13 -0600

    chore: add types for jshint using dts-gen

---
+++ types/jshint/index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for jshint 2.12
+// Project: http://jshint.com/
+// Definitions by: Amin Yahyaabadi <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export function JSHINT(s: any, o: any, g: any): any;
+
+export namespace JSHINT {
+    // Circular reference from jshint.JSHINT
+    const jshint: any;
+
+    function addModule(func: any): void;
+
+    function data(): any;
+
+}
+
